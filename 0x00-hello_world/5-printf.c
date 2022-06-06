e5e71f98d39276487ba0e56ffe2f787aa1c3cdc8
EphremGebretsion <ephhrem@gmail.com>
2022-06-06 07:09:14 -0400

    auto commited

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - this is used to execute everything
+ * the printf will print the coted parts
+ * Return: 0 after all that
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
