b748c1cd5a2ccc14b272d9f1f16455817d6ef354
EphremGebretsion <ephhrem@gmail.com>
2022-06-06 18:57:45 -0400

    auto commited

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+/**
+ * main - starting point
+ * print the number
+ * print the colen
+ * print the $ sighn at the end of the line
+ * Return: 0 at the end
+ */
+int main(void)
+{
+	int i = 48;
+	int t;
+	int c;
+
+	while (i < 57)
+	{
+		putchar(i);
+		t = 0;
+		c = 44;
+
+		while (t < 2)
+		{
+			putchar(c);
+			c = 32;
+			t++;
+		}
+		i++;
+	}
+	putchar(57);
+	c = 36;
+	t = 0;
+	while (t < 2)
+	{
+	putchar(c);
+	t++;
+	c = 10;
+	}
+	return (0);
+}
