425cf57e3b23b5e315e2294bd42f44d72220d853
EphremGebretsion <ephhrem@gmail.com>
2022-06-06 08:07:48 -0400

    auto commited

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,18 @@
 #include<stdio.h>
 /**
  * main - starts the program
+ * create char
+ * create int
+ * create long int
+ * create long long lint
+ * create a float
  * print size of char
  * print size of int
  * print size of long int
  * print size of long long int
  * print size of float
- * Return: return 0 after all that 
+ * Return: 0 returns zeroo after all that
+ *
  */
 int main(void)
 {
@@ -15,10 +21,11 @@ int main(void)
 	long int LI;
 	long long int LLI;
 	float F;
-	printf("Size of a char: %li byte(s)\n",sizeof(C));
-        printf("Size of an int: %li byte(s)\n",sizeof(I));
-	printf("Size of a long int: %li byte(s)\n",sizeof(LI));
-	printf("Size of a long long int: %li byte(s)\n",sizeof(LLI));
-	printf("Size of a float: %li byte(s)\n",sizeof(F));
-        return (0);
-}	
+
+	printf("Size of a char: %li byte(s)\n", sizeof(C));
+	printf("Size of an int: %li byte(s)\n", sizeof(I));
+	printf("Size of a long int: %li byte(s)\n", sizeof(LI));
+	printf("Size of a long long int: %li byte(s)\n", sizeof(LLI));
+	printf("Size of a float: %li byte(s)\n", sizeof(F));
+	return (0);
+}
