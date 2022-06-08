53f3af9b324cfcf0a82c4311da3d89622a972eba
EphremGebretsion <ephhrem@gmail.com>
2022-06-08 17:32:30 -0400

    auto commited

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,26 @@
 #include<stdio.h>
+#include "main.h"
 /**
  * main - starts everything
- * print using puts
+ * print using _putchar
  * Return: 0 at the end
  */
 int main(void)
 {
-	puts("_putchar");
+
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
+}
+int _putchar(char c)
+{
+	putchar(c);
+	return (1);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,13 +1,5 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 #include<stdio.h>
-void _putchar(char s[])
-{
-	int i = 0;
-	while(s[i] != '\0'){
-		putchar(s[i]);
-		i++;
-	}
-}
-void print_alphabet(void);
+int _putchar(char c);
 #endif
