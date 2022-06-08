5ad41d4df5986d13aac40ec5c3494ee721c0316d
EphremGebretsion <ephhrem@gmail.com>
2022-06-08 16:48:37 -0400

    auto commited

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * main - startung point
+ * print the alphabets in lower case using _putchar
+ * Return: 0 at the end
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	_putchar("\n");
+}
