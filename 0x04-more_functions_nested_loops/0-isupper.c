831e8b7384c9f0a2314850ddc2d378ea340afbb9
EphremGebretsion <ephhrem@gmail.com>
2022-06-09 16:39:16 -0400

    auto commited

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,12 +2,14 @@
 #include "main.h"
 /**
  * _isupper -is the function for upercase cheking
+ * @c: is used for accepting the character
+ *
  * check if it is upper or other
  * Return: 1 if its true or else 0
  */
 int _isupper(int c)
 {
-	if(c <= 90 && c >= 65)
+	if (c <= 90 && c >= 65)
 	{
 		return (1);
 	}
