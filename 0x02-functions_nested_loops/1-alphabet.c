013e372abd9cb4c7700e4941d31fe752ce11f1aa
EphremGebretsion <ephhrem@gmail.com>
2022-06-14 12:44:48 -0500

    commited by script auto commited

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,10 +7,10 @@ void print_alphabet(void)
 {
 	char ch = 'a';
 
-        while (ch <= 'z')
-        {
-                _putchar(ch);
-                ch++;
-        }
-        _putchar('\n');
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+	_putchar('\n');
 }
