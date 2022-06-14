9965104e90749fab68de7f5e9847a5a37d458c5b
EphremGebretsion <ephhrem@gmail.com>
2022-06-13 23:06:06 -0500

    commited by script auto commited

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,15 @@
+#include<string.h>
+#include "main.h"
+void rev_string(char *s)
+{
+	char *r = s;
+	int l = strlen(s) - 1;
+	int i = 0;
+	
+	while (l >= 0)
+	{
+		s[i] = r[l];
+		i++;
+		l--;
+	}
+}
