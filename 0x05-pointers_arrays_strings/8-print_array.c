983dc92dc890d2dfa7ef0b97c174937ca8ff0a02
EphremGebretsion <ephhrem@gmail.com>
2022-06-14 03:20:23 -0500

    commited by script auto commited

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,16 +1,17 @@
 #include<stdio.h>
 #include "main.h"
 /**
- * print_array - print the specified number of elements in the array starting from the first
+ * print_array - print the specified number of elements
  * @a: used to accept the array
  * @n: used to accept the ammount
  */
 void print_array(int *a, int n)
 {
 	int i = 0;
+
 	while (i < n)
 	{
-		printf("%i",a[i]);
+		printf("%i", a[i]);
 		i++;
 		if (i < n)
 		{
