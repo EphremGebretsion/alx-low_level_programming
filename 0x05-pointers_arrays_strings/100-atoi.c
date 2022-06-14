f533ca26cedfb9b09c1581e8437ca4b37d58935d
EphremGebretsion <ephhrem@gmail.com>
2022-06-14 04:39:43 -0500

    commited by script auto commited

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,12 @@
+#include<stdlib.h>
+/**
+ * _atoi - converts the string to int
+ * @s: is used to accept the string
+ * Return: returns the in value
+ */
+int _atoi(char *s)
+{
+	int i = atoi(s);
+
+	return (i);
+}
