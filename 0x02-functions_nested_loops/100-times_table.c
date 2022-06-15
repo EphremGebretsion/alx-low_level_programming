0e086ff39940e21b44ef3bfa72104c8cacde32c6
EphremGebretsion <ephhrem@gmail.com>
2022-06-15 14:13:46 -0500

    commited by script auto commited

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,72 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * psp - used to print ',' and the specified amount of space
+ * @s: accept the space ammount
+ */
+void psp(int s)
+{
+	int i = 0;
+
+	_putchar(',');
+	while (i < s)
+	{
+		_putchar(' ');
+		i++;
+	}
+}
+/**
+ * pnum - used to print an int
+ * @n: used to accept the number
+ */
+void pnum(int n)
+{
+	int r;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n > 9)
+	{
+		pnum(n / 10);
+	}
+	r = n % 10;
+	_putchar(r + 48);
+}
+/**
+ * print_times_table - prints the n number of time table
+ * @n: accept the number
+ */
+void print_times_table(int n)
+{
+	int a = 0, m = 0, s = 0, t;
+
+	t = n * n;
+	while (t >= 0)
+	{
+		t = t / 10;
+		s++;
+		if (t == 0)
+		{
+			t = -2;
+		}
+	}
+	while (a <= n)
+	{
+		m = 0;
+		while (m <= n)
+		{
+			t = m * n;
+			pnum(t);
+			if (m < n)
+			{
+				psp(s);
+			}
+			m++;
+			_putchar('\n');
+		}
+		a++;
+	}
+}
