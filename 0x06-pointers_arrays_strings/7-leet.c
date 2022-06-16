4b74757bc74ff85e8a4aa10dc59f2b3a61ac10b1
EphremGebretsion <ephhrem@gmail.com>
2022-06-16 14:31:13 -0500

    commited by script auto commited

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include<string.h>
+#include "main.h"
+/**
+ * leet - convert to 1337(leet)
+ * @s: accept the string
+ * Return: the converted string
+ */
+char *leet(char *s)
+{
+	int i = 0, l;
+
+	l = strlen(s);
+	while (i < l)
+	{
+		if (s[i] == 'a' || s[i] == 'A')
+			s[i] = '4';
+		else if (s[i] == 'e' || s[i] == 'E')
+			s[i] = '3';
+		else if (s[i] == 'o' || s[i] == 'O')
+			s[i] = '0';
+		else if (s[i] == 't' || s[i] == 'T')
+			s[i] = '7';
+		else if (s[i] == 'l' || s[i] == 'L')
+			s[i] = '1';
+		i++;
+	}
+	return (s);
+}
