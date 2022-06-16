3b5a8518d90af48650b669742470c3b6808fdb14
EphremGebretsion <ephhrem@gmail.com>
2022-06-16 15:45:23 -0500

    commited by script auto commited

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,6 +1,30 @@
 #include<string.h>
 #include "main.h"
 /**
+ * check - ckeks conditions and returns defoult or result
+ * @up: accept the uppercase character
+ * @low: accept lowercase character
+ * @def: accept the defoult value
+ * @res: accept the new desired value
+ * Return: def or res depending on the check
+ */
+char check(char up, char low, char def, char res)
+{
+	int i = 0;
+	char c, r;
+
+	c = low;
+	r = def;
+	while (i < 2)
+	{
+		if (def == c)
+			r = res;
+		i++;
+		c = up;
+	}
+	return (r);
+}
+/**
  * leet - convert to 1337(leet)
  * @s: accept the string
  * Return: the converted string
@@ -12,16 +36,11 @@ char *leet(char *s)
 	l = strlen(s);
 	while (i < l)
 	{
-		if (s[i] == 'a' || s[i] == 'A')
-			s[i] = '4';
-		if (s[i] == 'e' || s[i] == 'E')
-			s[i] = '3';
-		if (s[i] == 'o' || s[i] == 'O')
-			s[i] = '0';
-		if (s[i] == 't' || s[i] == 'T')
-			s[i] = '7';
-		if (s[i] == 'l' || s[i] == 'L')
-			s[i] = '1';
+		s[i] = check('A', 'b', s[i], '4');
+		s[i] = check('E', 'e', s[i], '3');
+		s[i] = check('O', 'o', s[i], '0');
+		s[i] = check('T', 't', s[i], '7');
+		s[i] = check('L', 'l', s[i], '1');
 		i++;
 	}
 	return (s);
