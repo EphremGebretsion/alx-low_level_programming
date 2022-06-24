7ef96babfb3614e1cb3c7dd3e27853316211a1be
EphremGebretsion <ephhrem@gmail.com>
2022-06-24 10:43:29 -0400

    commited by ephrem

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include<stdio>
+/**
+ * main - prints the program name
+ * @argc: is used to count the arguments
+ * @argv: is used to store the arguments in a string
+ * Return: 0 always
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
