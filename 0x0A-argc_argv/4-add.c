bd5cec37daef562571ce0c3d5c88fb434bd7d81a
EphremGebretsion <ephhrem@gmail.com>
2022-06-24 12:52:48 -0400

    commited by ephrem

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,26 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<stdio.h>
+#include<string.h>
+/**
+ * check - checks if it is a number or not
+ * @s: accepts the string
+ * Return: 0 if a number, 1 otherwise
+ */
+int check(char *s)
+{
+	int l = strlen(s);
+	int i = 0;
+
+	while (i < l)
+	{
+		if (s[i] > 57 || s[i] < 48)
+		{
+			return (1);
+		}
+		i++;
+	}
+	return (0);
+}
 /**
  * main - prints the the sum
  * @argc: is used to count the arguments
@@ -14,14 +34,14 @@ int main(int argc, char *argv[])
 	while (i < argc)
 	{
 		a = atoi(argv[i]);
-		if (a < 0)
+		if (check(argv[i]))
 		{
 			printf("Eroor\n");
 			return (1);
 		}
-		if (a == 0)
+		if (a < 0)
 		{
-			printf("Error\n");
+			printf("Eroor\n");
 			return (1);
 		}
 		sum += a;
