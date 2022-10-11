fd8c89546aef07b47917ec24cb715692b5b2f7fa
EphremGebretsion <ephhrem@gmail.com>
2022-10-11 17:14:34 -0400

    done

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+/**
+ * init_dog - is used to intialize dog
+ * @d: accepts the address of dog instance
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the dog owner
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
