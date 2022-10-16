6f382c9cc1a271b48e3154017388db9a530e9f08
EphremGebretsion <ephhrem@gmail.com>
2022-10-16 15:26:52 -0400

    array iterator is done

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+#include <stddef.h>
+/**
+ * array_iterator - is used to iterate and excute the function
+ * @array: is the array to be iterated
+ * @size: the size of the array
+ * @action: the function to be excuted
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
