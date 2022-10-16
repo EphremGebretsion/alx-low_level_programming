38da3ee849287a75a4fb69bc79bddb808cb0c502
EphremGebretsion <ephhrem@gmail.com>
2022-10-16 12:34:03 -0400

    done for funtion pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - is used to call the print name funtions usint the name
+ * @name: is the name
+ * @f: is the function used to print the name
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
