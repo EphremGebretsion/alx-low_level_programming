51891e6407c994ccb99ddf4b5c221eddfe7d6e7d
EphremGebretsion <ephhrem@gmail.com>
2022-10-16 15:35:30 -0400

    array iterator is done

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,9 +1,11 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
+#include <stddef.h>
 
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void print_name_as_is(char *name);
 void print_name_uppercase(char *name);
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif
