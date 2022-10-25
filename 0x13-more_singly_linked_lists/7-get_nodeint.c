bde6356d6fc78309a0c08801a6d6760b68db421f
EphremGebretsion <ephhrem@gmail.com>
2022-10-25 16:46:40 -0400

    finding the node at the given index

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -8,13 +8,13 @@
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	listint_t *t = head;
 	unsigned int i = 0;
-	
+	listint_t *t = head;
+
 	while (i != index && t != NULL)
 	{
 		t = t->next;
-		i++;
+		i++
 	}
 	if (t == NULL)
 		return (NULL);
