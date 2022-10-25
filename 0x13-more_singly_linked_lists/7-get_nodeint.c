4944c09247f0b8cd4f2a3ae07f09c1223f8c6ed0
EphremGebretsion <ephhrem@gmail.com>
2022-10-25 16:40:14 -0400

    finding the node at the given index

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - find the node at the ginven index
+ * @head: the head of the linked list
+ * @index: the index of the node
+ * Return: return the node if found else return NULL
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *t = head;
+	unsigned int i = 0;
+	
+	while (i != index && t != NULL)
+	{
+		t = t->next;
+		i++;
+	}
+	if (t == NULL)
+		return (NULL);
+	return (t);
+}
