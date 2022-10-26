e1280e0da4aa535afebcaf47de2178e1cc3999a2
EphremGebretsion <ephhrem@gmail.com>
2022-10-25 20:36:45 -0400

    reverse linked list

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,6 +1,25 @@
 #include <stdlib.h>
 #include "lists.h"
 /**
+ * add_nodebeg - adds a new node infront of the current head
+ * @head: address of the head pointer value
+ * @n: the value of the intiger value
+ * Return: the new head value
+ */
+
+listint_t *add_nodebeg(listint_t **head, const int n)
+{
+	listint_t *t = malloc(sizeof(listint_t));
+
+	if (t == NULL)
+		return (NULL);
+	t->n = n;
+	t->next = *head;
+	*head = t;
+	return (t);
+}
+
+/**
  * reverse_listint - rerse the linked list
  * @head: the head of the linked list
  * Return: return the head of the reversed linked list
@@ -12,7 +31,7 @@ listint_t *reverse_listint(listint_t **head)
 
 	while (temp != NULL)
 	{
-		add_nodeint(&t, temp->n);
+		add_nodebeg(&t, temp->n);
 		temp = temp->next;
 	}
 	*head = t;
