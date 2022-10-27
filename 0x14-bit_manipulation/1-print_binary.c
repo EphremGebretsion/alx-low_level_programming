ce91aa6a347220e383b1b0cedd30b78fb3a7122d
EphremGebretsion <ephhrem@gmail.com>
2022-10-27 17:17:23 -0400

    printing the binary of the decimal number

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,56 @@
+#include "main.h"
+
+/**
+ * po - used to calculate the power of the number
+ * @b: the base number
+ * @p: the power number
+ * Return: the result of b to the power p
+ */
+unsigned int po(unsigned int b, unsigned int p)
+{
+	unsigned int i = 0, res = 1;
+
+	if (p == 0)
+		return (1);
+	for (; i < p; i++)
+		res = res * b;
+	return (res);
+}
+/**
+ * print_binary - prints the binary of the decimal number
+ * @n: the decimal number
+ * Return: return nothing
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int r = 1, m = n;
+	int i = -1;
+
+	if (n == 0)
+	{
+		_putchar('0');
+	}
+	else
+	{
+		do {
+			i++;
+			r = po(2, i);
+			if (r > n)
+			{
+				i--;
+				break;
+			}
+		} while (r != n);
+		for (; i >= 0; i--)
+		{
+			r = po(2, i);
+			if (r <= m)
+			{
+				_putchar('1');
+				m -= r;
+			}
+			else
+				_putchar('0');
+		}
+	}
+}
