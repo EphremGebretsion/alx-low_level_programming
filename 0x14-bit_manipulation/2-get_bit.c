1a569c49150e54331b35f34b71220bf045b1ba39
EphremGebretsion <ephhrem@gmail.com>
2022-10-30 09:08:56 -0400

    return the bit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,59 @@
+#include "main.h"
+/**
+ * po - used to calculate the power of the number
+ * @p: the power number
+ * Return: the result of b to the power p
+ */
+unsigned long int po(unsigned int p)
+{
+	unsigned long int res = 1;
+	unsigned int i;
+
+	i = 0;
+	if (p == 0)
+		return (1);
+	for (; i < p; i++)
+		res = res * 2;
+	return (res);
+}
+/**
+ * get_bit - used to get the bit value the given index
+ * @n: the number
+ * @index: the index for the bit
+ * Return: the bit value or -1
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int s = 8 * sizeof(unsigned long int), r;
+	unsigned long int m = n;
+	int j, i = 0;
+
+	if (index >= s)
+		return (-1);
+	if (n == 0 && index == 0)
+		return (0);
+	do {
+		r = po(i);
+		if (r > n)
+		{
+			i--;
+			break;
+		}
+		i++;
+	} while (r != n && i != s);
+	for (; i >= 0; i--)
+	{
+		j = 0;
+		r = po(i);
+		if (r <= m)
+		{
+			m -= r;
+			j = 1;
+		}
+		else
+			j = 0;
+		if (i == index)
+			return (j);
+	}
+	return (0);
+}
