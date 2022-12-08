73866f294d64acdfe2b6db76acb969ca78c02b5d
= <=>
2022-12-09 01:29:35 +0300

    inseting in to a list

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -14,7 +14,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new_prev;
 	dlistint_t *new_node = malloc(sizeof(dlistint_t));
 
-	if (new_node == NULL)
+	if (h == NULL || *h == NULL || new_node == NULL)
 		return (NULL);
 	new_node->n = n;
 	if (idx == 0)
