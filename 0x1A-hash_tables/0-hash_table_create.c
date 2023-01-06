3b9960cb9d18a80842b145a48cba3aa366100511
= <=>
2023-01-06 16:18:49 +0300

    create hash table n

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,13 +8,19 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	unsigned long int i = 0;
 	hash_table_t *new_hash_table = (hash_table_t *) malloc(sizeof(hash_table_t));
 
 	if (new_hash_table == NULL)
 		return (NULL);
 	new_hash_table->size = size;
-	new_hash_table->array = (hash_node_t **) calloc(sizeof(hash_node_t), size);
+	new_hash_table->array = (hash_node_t **) calloc(size, sizeof(hash_node_t));
 	if (new_hash_table->array == NULL)
 		return (NULL);
+	while (i < new_hash_table->size)
+	{
+		new_hash_table->array[i] = NULL;
+		i++;
+	}
 	return (new_hash_table);
 }
